c84dfc38721d05440aebfb43b1a09f0b0e1b8d61
CaptainAarav <aaravsahni1037@gmail.com>
2026-09-10 21:00:29 +0100

    Added new --verbose arg for extra details and added rich library for md terminal rendering

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
+from rich.console import Console
+from rich.markdown import Markdown  
 
 # loading the api key from .env
 load_dotenv()
@@ -11,11 +13,22 @@ api_key: str = os.environ.get("OPENROUTER_API_KEY")
 if api_key is None:
     raise RuntimeError("api key could not be loaded")
 
+# sets up console object for printing markdown
+console = Console()
+
+# initialise a new argparse parser so we can use the run commands arguments
 parser = argparse.ArgumentParser(description="ai coding agent")
 parser.add_argument("user_prompt", type=str, help="User Prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
+# set the user prompt using argument parser
 user_prompt = args.user_prompt
+
+# a new messages list that will store the entire conversations messages
+messages = [
+    {"role": "user", "content": user_prompt},
+]
 
 # create a new client that talks to openrouter api
 client = OpenAI(
@@ -26,17 +39,18 @@ client = OpenAI(
 # creates a new chat completions request with our user prompt
 response = client.chat.completions.create(
     model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": user_prompt,
-        }
-    ],
+    messages=messages,
 )
 
-print(f"User prompt: {user_prompt}")
-print(f"Model used: {response.model}")
-print(f"Prompt tokens: {response.usage.prompt_tokens}")
-print(f"Response tokens: {response.usage.completion_tokens}")
-print(f"Total tokens: {response.usage.total_tokens}")
-print(f"Response:\n{response.choices[0].message.content}")
+formatted_response = Markdown(response.choices[0].message.content)
+
+# checks whether --verbose arg was added
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Model used: {response.model}")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Total tokens: {response.usage.total_tokens}") 
+    
+# prints the formatted md using rich library
+console.print(formatted_response)
