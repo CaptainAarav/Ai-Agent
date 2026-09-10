96326bc2a2a8158666bc534edb7ba63a1e26ec49
CaptainAarav <aaravsahni1037@gmail.com>
2026-09-10 21:36:24 +0100

    Added rich formatting to output

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from rich.console import Console
 from rich.markdown import Markdown  
+from rich.panel import Panel
+from rich.table import Table
 
 # loading the api key from .env
 load_dotenv()
@@ -13,8 +15,9 @@ api_key: str = os.environ.get("OPENROUTER_API_KEY")
 if api_key is None:
     raise RuntimeError("api key could not be loaded")
 
-# sets up console object for printing markdown
+# sets up console object for printing markdown and add a title bar to top of terminal
 console = Console()
+console.rule("[bold]AI Coding Agent")
 
 # initialise a new argparse parser so we can use the run commands arguments
 parser = argparse.ArgumentParser(description="ai coding agent")
@@ -36,21 +39,32 @@ client = OpenAI(
     api_key=api_key
 )
 
-# creates a new chat completions request with our user prompt
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=messages,
-)
+# creates a new chat completions request with our user prompt and using rich console to show a loading spinning circle
+with console.status("Thinking..."):
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
 
+# formats response content to md
 formatted_response = Markdown(response.choices[0].message.content)
 
 # checks whether --verbose arg was added
 if args.verbose:
-    print(f"User prompt: {user_prompt}")
-    print(f"Model used: {response.model}")
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
-    print(f"Total tokens: {response.usage.total_tokens}") 
-    
-# prints the formatted md using rich library
-console.print(formatted_response)
+    # prints all the extra details with formatting using rich
+    console.print(f"User Prompt: [bold white]{user_prompt}[/bold white]", justify="center")
+    table = Table(title="Stats", expand=False)
+    table.add_column("Model")
+    table.add_column("Prompt Tokens", justify="right")
+    table.add_column("Response Tokens", justify="right")
+    table.add_column("Total Tokens", justify="right")
+    table.add_row(
+        response.model,
+        str(response.usage.prompt_tokens),
+        str(response.usage.completion_tokens),
+        str(response.usage.total_tokens),
+    )
+    console.print(table, justify="center")
+
+# prints response content even if --verbose is not set
+console.print(Panel(formatted_response, title="[bold white]Response[/bold white]", border_style="cyan"))
